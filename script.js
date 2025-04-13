c9ec0e0512e0b3e6ae404934a2722943baa5b905
thiago souza <thiagosouzadasilva78@gmail.com>
2025-04-13 16:51:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+function updateClock() {
+    let now = new Date();
+
+    let secund = now.getSeconds();
+    let minute = now.getMinutes();
+    let hour = now.getHours();
+    
+    digitalElement.innerText = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(secund)}`
+    let sDeg = ((360 / 60) * secund ) - 90 ;
+    let mDeg = ((360 / 60) * minute ) - 90 ;
+    let hDeg = ((360 / 12) * minute ) - 90 ;
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
+};
+function fixZero(time) {
+    if(time < 10) {
+        return '0'+time;
+    } else {
+        return time;
+    }
+//     return time < 10 ? `0${time}`: time;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
